71b3cbdbebf0242bfe821b016818d7192800cc33
EvgeniyaNesterkova <miledieva08@gmail.com>
2021-10-27 00:35:26 +0300

    Реализовала снятие выделения с файла и ссылки при клике вне этого файла/ссылки

--- src/components/FileComp.vue
+++ src/components/FileComp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div @click="select" :class="{selected} " class="file">
+  <div class="file" tabindex="1">
       <img width="35px" src="../assets/img/fileIcon.svg" alt="Иконка файла">
       <span>
           {{fileName}}
@@ -15,27 +15,16 @@ export default {
             type: String,
             default: ''
         }
-    },
-    data(){
-        return{
-            selected: false,
-        }
-    },
-    methods: {
-        select(){
-            this.selected = !this.selected;
-        }
-    },
+    }
 }
 </script>
 
 <style scoped>
-    .selected{
-        background-color: rgb(153, 204, 255, 0.5);;
-    }
     .file{
         width: fit-content;
         padding: 5px;
     }
-        
+    .file:focus-within {
+        background-color: rgb(153, 204, 255, 0.5);
+    }    
 </style>
--- src/components/LinkComp.vue
+++ src/components/LinkComp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div @click="select" :class="{selected} " class="link">
+  <div class="link" tabindex="1">
       <img width="35px" src="../assets/img/linkIcon.svg" alt="Иконка папки">
       <a v-bind:href="target">
           {{linkName}}
@@ -19,27 +19,16 @@ export default {
             type: String,
             default: '#'
         }
-    },
-    data(){
-        return{
-            selected: false,
-        }
-    },
-    methods: {
-        select(){
-            this.selected = !this.selected;
-        }
-    },
+    }
 }
 </script>
 
 <style scoped>
-    .selected{
-        background-color: rgb(153, 204, 255, 0.5);
-    }
     .link{
         width: fit-content;
         padding: 5px;
     }
-        
+    .link:focus-within {
+        background-color: rgb(153, 204, 255, 0.5);
+    }     
 </style>
